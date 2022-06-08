f5df9bff46fecedda9b60ba69f8ff9ab47a759a8
Madison <madisonthompsonld@gmail.com>
2022-06-08 15:43:40 -0700

    Added clear-any-console

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node   
 //above code only works on mac
 
+const clearConsole = require('clear-any-console');
+ 
+// Clears the console.
+clearConsole();
+
 console.log('Jamie Thompson https://github.com/thommadison'); 
